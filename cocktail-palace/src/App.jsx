c80db0068d0eb38269d3ef3a3407c3d2ece3d4da
nunjayi <epenyash@gmail.com>
2024-05-10 11:28:00 +0300

    setting up the project

---
+++ cocktail-palace/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './styles/App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <>
+        <p>welcome</p>
+   </>
+
+  )
+}
+
+export default App
